c84bbcfa7f93feb00443494259f67153efe19a74
WebShining <webshinings@protonmail.com>
2023-11-25 03:03:26 +0200

    updated config

--- core/config.py
+++ core/config.py
@@ -4,13 +4,15 @@ env = Env()
 env.read_env()
 
 PORT = env.int('PORT', 4000)
+HOST = env.str('HOST', "127.0.0.1")
 
-POSTGRES_USER = env.str('POSTGRES_USER', default=None)
-POSTGRES_PASS = env.str('POSTGRES_PASS', default=None)
-POSTGRES_HOST = env.str('POSTGRES_HOST', default=None)
-POSTGRES_PORT = env.int('POSTGRES_PORT', default=None)
-POSTGRES_NAME = env.str('POSTGRES_NAME', default=None)
+DB_NAME = env.str('DB_NAME', default=None)
+DB_USER = env.str('DB_USER', default=None)
+DB_PASS = env.str('DB_PASS', default=None)
+DB_HOST = env.str('DB_HOST', default=None)
+DB_PORT = env.int('DB_PORT', default=None)
+
 
 DATABASE_URI = env.str('DATABASE_URI', default='sqlite+aiosqlite:///database.sqlite3')
-if POSTGRES_USER and POSTGRES_PASS and POSTGRES_HOST and POSTGRES_PORT and POSTGRES_NAME:
-    DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_NAME}'
+if DB_HOST and DB_PORT and DB_USER and DB_PASS and DB_NAME:
+    DATABASE_URI = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import router as api_router
+from core.config import PORT, HOST
 from core.handlers import shutdown_app_handler, start_app_handler
 
 app = FastAPI()
@@ -19,6 +20,5 @@ app.add_event_handler("startup", start_app_handler(app))
 app.add_event_handler("shutdown", shutdown_app_handler(app))
 app.include_router(api_router, prefix="/api")
 
-
 if __name__ == '__main__':
-    uvicorn.run('server:app', reload=True, port=4000)
+    uvicorn.run('server:app', reload=True, port=PORT, host=HOST)
